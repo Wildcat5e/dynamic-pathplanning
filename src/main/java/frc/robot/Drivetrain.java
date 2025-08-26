3acb79fe0cb2a633384f71dd0b7192f030c6cfd0
Tom Gagnier <tomgagnier@gmail.com>
2025-08-26 17:04:41 -0400

    Format code, remove superfluous comments and prints

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,47 +1,43 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
 
-/** Add your docs here. */
+import static com.revrobotics.spark.SparkBase.PersistMode.kPersistParameters;
+import static com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters;
+import static com.revrobotics.spark.SparkLowLevel.MotorType.kBrushed;
+
 public class Drivetrain extends SubsystemBase {
-    private final SparkMax leftLeader = new SparkMax(2, MotorType.kBrushed);
-    private final SparkMax leftFollower = new SparkMax(1, MotorType.kBrushed);
-    private final SparkMax rightLeader = new SparkMax(3, MotorType.kBrushed);
-    private final SparkMax rightFollower = new SparkMax(4, MotorType.kBrushed);
+    private final SparkMax leftLeader = new SparkMax(2, kBrushed);
+    private final SparkMax leftFollower = new SparkMax(1, kBrushed);
+    private final SparkMax rightLeader = new SparkMax(3, kBrushed);
+    private final SparkMax rightFollower = new SparkMax(4, kBrushed);
+
     private final DifferentialDrive drivetrain = new DifferentialDrive(leftLeader::set, rightLeader::set);
 
-public Drivetrain() {
-
-    System.out.println("in drivetrain constructor");
-
-    SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
-    leftFollowerConfig.follow(leftLeader.getDeviceId());
-
-    SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
-    rightFollowerConfig.follow(rightLeader.getDeviceId());
-    rightFollowerConfig.inverted(true);
-
-
-    leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-System.out.println("leaving drivetrain constructor");
-
-}
-
-public void drive(double xSpeed, double zRotation){
-    drivetrain.arcadeDrive(xSpeed, zRotation);
-}
+    public Drivetrain() {
+        leftLeader.configure(
+                new SparkMaxConfig().inverted(false),
+                kResetSafeParameters, kPersistParameters
+        );
+        leftFollower.configure(
+                new SparkMaxConfig().inverted(false).follow(leftLeader.getDeviceId()),
+                kResetSafeParameters, kPersistParameters
+        );
+        rightLeader.configure(
+                new SparkMaxConfig().inverted(true),
+                kResetSafeParameters, kPersistParameters
+        );
+        rightFollower.configure(
+                new SparkMaxConfig().inverted(true).follow(rightLeader.getDeviceId()),
+                kResetSafeParameters, kPersistParameters
+        );
+    }
 
 
+    public void drive(double xSpeed, double zRotation) {
+        drivetrain.arcadeDrive(xSpeed, zRotation);
+    }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,44 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 
-
-/**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
- * the code necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
-  private final XboxController xboxController = new XboxController(0);
+    private final XboxController xboxController = new XboxController(0);
 
-Drivetrain drivetrain = new Drivetrain();
+    Drivetrain drivetrain = new Drivetrain();
 
-
-
-  /** Called once at the beginning of the robot program. */
-  public Robot() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    
-System.out.println("robot constructor");
-
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    drivetrain.drive(-xboxController.getLeftY(), xboxController.getRightY());
-  }
+    @Override
+    public void teleopPeriodic() {
+        drivetrain.drive(-xboxController.getLeftY(), xboxController.getRightY());
+    }
 }
