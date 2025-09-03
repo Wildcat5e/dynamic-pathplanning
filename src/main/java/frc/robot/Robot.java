7ad393d243f43fcf40717a16ed0ccd5e2e9aade4
wildcat6705 <147210783+wildcat6705@users.noreply.github.com>
2025-09-03 17:44:43 -0400

    gyroscope debug working

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,37 @@
 package frc.robot;
 
+import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Robot extends TimedRobot {
-    private final XboxController xboxController = new XboxController(0);
+    private final CommandXboxController xboxController = new CommandXboxController(0);
 
+    private final Pigeon2 gyroscope = new Pigeon2(0, "rio");
     Drivetrain drivetrain = new Drivetrain();
+
+    public Robot() {
+        xboxController.a().whileTrue(Commands.runOnce(() -> System.out.println(gyroToString())));
+    }
 
     @Override
     public void teleopPeriodic() {
         drivetrain.arcadeDrive(xboxController.getLeftY(), xboxController.getRightX());
-        //drivetrain.tankDrive(xboxController.getLeftY(), xboxController.getRightY());
+    }
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+    String gyroToString() {
+        System.out.println("world");
+        return 
+            "rotation2d = " + gyroscope.getRotation2d() +
+            "\naccum x" + gyroscope.getAccumGyroX();
     }
 }
